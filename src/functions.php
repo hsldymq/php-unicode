9cb2305be563d252685e5bde02ba4f246545ca4c
hsldymq <hsldymq@qq.com>
2019-05-23 15:31:51 +0800

    add function

---
+++ src/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Archman\Unicode;
+
+/**
+ * 将UTF-8编码的字符串转换为unicode code point数组.
+ *
+ * @param string $str
+ *
+ * @return int[]
+ */
+function unicode(string $str): array
+{
+    // TODO
+    return [];
+}
